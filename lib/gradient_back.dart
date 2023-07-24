739d4f74e4cde1efe400bf9becee78ab601abaa4
Mateo Mercado <matemerc@gmail.com>
2023-07-24 16:46:06 -0400

    Added gradient for the images background

--- lib/gradient_back.dart
+++ lib/gradient_back.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
 
 import 'package:flutter/material.dart';
 
 class GradientBack extends StatelessWidget {
+  String title = "Popular";
+  GradientBack(this.title, {super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -11,11 +14,17 @@ class GradientBack extends StatelessWidget {
       height: 250.0,
       decoration: BoxDecoration(
           gradient: LinearGradient(
-              colors: [Colors.blue, Colors.purple],
+              colors: [Colors.blue, Colors.green],
               begin: FractionalOffset(0.2, 0.0),
               end: FractionalOffset(1.0, 0.6),
               stops: [0.0, 0.6],
               tileMode: TileMode.clamp)),
+      child: Text(
+        title,
+        style: TextStyle(
+            color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),
+      ),
+      alignment: Alignment(-0.9, -0.6),
     );
 
     return bgGradient;
